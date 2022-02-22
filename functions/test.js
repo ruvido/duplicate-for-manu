b14828146e5a8854fd34876337351119e28e2698
Francesco Rao <ruvido@gmail.com>
2022-02-22 21:14:16 +0100

    Daily

--- functions/test.js
+++ functions/test.js
@@ -62,4 +62,4 @@ const handler = async function(event, context) {
         //        "\n    sendNewsletter: "+sendNewsletter
     }
 }
-module.exports.handler = schedule("@hourly", handler);
+module.exports.handler = schedule("@daily", handler);
